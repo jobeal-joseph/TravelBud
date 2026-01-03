78881090937cf2307df23b1182eda013f9c8393f
Jobeal M Joseph <jobeal2020@gmail.com>
2026-01-03 13:16:35 +0530

    authentication updation

--- backend/config.py
+++ backend/config.py
@@ -2,5 +2,5 @@
 
 class Config:
     SECRET_KEY = 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
