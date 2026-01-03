5235a273d0f39e144ff19289368d1ad6bb3af86d
Jobeal M Joseph <jobeal2020@gmail.com>
2026-01-03 11:40:45 +0530

    resolved an issue

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../frontend/templates')
 app.config.from_object('config.Config')
 
 CORS(app)
 db = SQLAlchemy(app)
 
+@app.route('/')
+def home():
+    return render_template('login.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
+
